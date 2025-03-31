42bc67d7f10b509197ef91eb99db29b9582e7bff
JISUNG PARK <meowbutlerdev@gmail.com>
2025-03-31 09:54:23 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 16.4 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,14 +1,10 @@
 import Foundation
 
-func solution(_ brown:Int, _ yellow:Int) -> [Int] {
+func solution(_ brown: Int, _ yellow: Int) -> [Int] {
     let area = brown + yellow
-    for height in 1...Int(sqrt(Double(area))) {
-        if area % height == 0 {
-            let width = area / height
-            if (width - 2) * (height - 2) == yellow {
-                return [width, height]
-            }
-        }
+    for height in 1...Int(sqrt(Double(area))) where area % height == 0 {
+        let width = area / height
+        if (width - 2) * (height - 2) == yellow { return [width, height] }
     }
     
     return []
